77a6d37937e0680feb1485cd8159f9438c8e639b
Mary Okosun <maryokosun@Marys-MacBook-Pro.local>
2022-01-13 15:22:15 +0100

    user authentication

--- migrations/seeds/sqls/20220113094126-role-seed-up.sql
+++ migrations/seeds/sqls/20220113094126-role-seed-up.sql
@@ -1,2 +1,3 @@
 /* Replace with your SQL commands */
-INSERT INTO roles (role_code,role_name) VALUES ('ADM','ADMIN');
+INSERT INTO roles (role_code,role_name) 
+VALUES ('ADM','ADMIN');
--- migrations/sqls/20220113080636-add-user-role-up.sql
+++ migrations/sqls/20220113080636-add-user-role-up.sql
@@ -1,8 +1,8 @@
 /* Replace with your SQL commands */
 CREATE TABLE IF NOT EXISTS roles(
-  "id" SERIAL PRIMARY KEY,
-  "role_code" varchar not null UNIQUE,
-  "role_name" varchar not null,
-  "created_at" timestamp default now(),
-  "updated_at" timestamp default now()
+  id SERIAL PRIMARY KEY,
+  role_code varchar not null UNIQUE,
+  role_name varchar not null,
+  created_at timestamp default now(),
+  updated_at timestamp default now()
 );
